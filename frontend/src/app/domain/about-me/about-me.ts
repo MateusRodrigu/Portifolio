f2b2c28aee68a0397bb4351d3bf97f97b2f66875
MateusRodrigu <164748740+MateusRodrigu@users.noreply.github.com>
2025-08-28 19:01:48 -0300

    tete
    teste

--- frontend/src/app/domain/about-me/about-me.ts
+++ frontend/src/app/domain/about-me/about-me.ts
@@ -19,21 +19,21 @@ export class AboutMe {
   texts = {
     pt: {
       title: 'Olá, eu sou',
-      name: 'Nome',
-      subtitle: 'Sou um desenvolvedor Front-end com 10 anos de experiência',
-      presentation: 'Olá! Sou um desenvolvedor full-stack apaixonado, com mais de 5 anos de experiência na criação de soluções digitais que geram impacto real. Atualmente estou no 4º período da faculdade de Engenharia de Software buscando sempre me desenvolver ainda mais.',
+      name: 'Mateus Costa',
+      subtitle: 'Sou um desenvolvedor em desenvolvimento a 3 anos de estudo e alto desenvolvimento',
+      presentation: 'Olá! Sou um desenvolvedor em desenvolvimento, 3 anos de estudo e alto desenvolvimento na criação de soluções digitais que geram impacto real. Atualmente estou no 4º período da faculdade de Engenharia de Software buscando sempre me desenvolver ainda mais.',
       buttonWork: 'Conheça mais sobre meu trabalho',
       buttonCV: 'Baixar CV',
       sectionTitle: 'Sobre mim'
     },
     en: {
-      title: 'Hi, I am',
-      name: 'Name',
-      subtitle: 'I am a Front-end developer with 10 years of experience',
-      presentation: 'Hi! I am a passionate full-stack developer with more than 5 years of experience creating digital solutions that make a real impact. I am currently in the 4th semester of Software Engineering, constantly striving to improve my skills.',
-      buttonWork: 'Check out my work',
+      title: 'Hello, I am',
+      name: 'Mateus Costa',
+      subtitle: 'I am a developer in development with 3 years of study and self-improvement',
+      presentation: 'Hello! I am a developer in development, with 3 years of study and strong self-improvement in creating digital solutions that generate real impact. I am currently in the 4th semester of Software Engineering, always striving to further improve myself.',
+      buttonWork: 'Learn more about my work',
       buttonCV: 'Download CV',
-      sectionTitle: 'About Me'
+      sectionTitle: 'About me'
     }
   };
 
